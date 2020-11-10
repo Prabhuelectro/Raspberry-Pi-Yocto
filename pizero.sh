776865a3cc1ed46104ac3e0a7de27e3e43e65d63
Prabhu <prabhuelectro99@gmail.com>
2020-11-10 16:45:33 +0530

    Update pizero.sh

--- pizero.sh
+++ pizero.sh
@@ -1,6 +1,7 @@
 sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3 xterm -y
-export BRANCH="warrior"  
-mkdir mender-raspberrypi && cd mender-raspberrypi
+#export BRANCH="warrior"  
+export BRANCH="zeus"
+mkdir raspberrypi && cd raspberrypi
 repo init -u https://github.com/mendersoftware/meta-mender-community -m meta-mender-raspberrypi/scripts/manifest-raspberrypi.xml -b ${BRANCH}
 repo sync
 source setup-environment raspberrypi
