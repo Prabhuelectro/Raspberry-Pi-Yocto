29039b287ce8d54cdee0e6e2ab9a61e9b40fe55e
Prabhu <prabhuelectro99@gmail.com>
2020-11-10 16:37:21 +0530

    Create pizero.sh
    Build Script for raspberry pi zero W

---
+++ pizero.sh
@@ -0,0 +1,7 @@
+sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3 xterm -y
+export BRANCH="warrior"  
+mkdir mender-raspberrypi && cd mender-raspberrypi
+repo init -u https://github.com/mendersoftware/meta-mender-community -m meta-mender-raspberrypi/scripts/manifest-raspberrypi.xml -b ${BRANCH}
+repo sync
+source setup-environment raspberrypi
+MACHINE=raspberrypi0-wifi bitbake core-image-base
